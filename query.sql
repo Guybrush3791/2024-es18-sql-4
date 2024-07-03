8f7d755f1cb3c8f49156424ab5bf70763ff09d26
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-03 15:20:50 +0200

    update

--- query.sql
+++ query.sql
@@ -2,112 +2,106 @@
 
 -- Esercizi solo con SELECT
 -- 1. Seleziona tutti i professori dal dipartimento di Informatica.
--- res: 
+-- res: 1
 
 
 
 -- 2. Elenca tutti gli studenti iscritti nell'anno 2021.
--- res: 
+-- res: 2
 
 
 
 -- 3. Visualizza tutti i corsi offerti dal dipartimento di Fisica.
--- res: 
+-- res: 1
 
 
 
 -- 4. Mostra i nomi dei corsi insieme ai relativi dipartimenti.
--- res: 
+-- res: 10
 
 
 
 -- 5. Conta il numero totale di studenti iscritti all'università.
--- res: 
+-- res: 1 (value: 10)
 
 
 
 -- Esercizi con GROUP BY e HAVING
--- res: 
-
-
-
 -- 6. Conta quanti corsi sono offerti in ogni dipartimento.
--- res: 
+-- res: 10 (uno per dipartimento)
 
 
 
+-- DELETED
 -- 7. Trova i dipartimenti che hanno più di 3 corsi.
 -- res: 
+-- DELETED
 
 
 
 -- 8. Elenca il numero di studenti iscritti per ogni anno di iscrizione.
--- res: 
+-- res: 5 (3, 2, 2, 2, 1)
 
 
-
+-- DELETED
 -- 9. Mostra il numero medio di studenti per corso in ogni dipartimento.
 -- res: 
+-- DELETED
 
-
-
+-- DELETED
 -- 10. Trova i professori che insegnano più di un corso.
 -- res: 
-
+-- DELETED
 
 
 -- Esercizi con JOIN
--- res: 
-
-
-
 -- 11. Mostra il nome di ogni studente insieme al nome del corso a cui sono iscritti.
--- res: 
+-- res: 30
 
 
 
 -- 12. Elenca tutti i corsi insieme ai nomi dei professori che li insegnano.
--- res: 
+-- res: 10
 
 
 
 -- 13. Trova gli studenti e i corsi relativi al dipartimento di Chimica.
--- res: 
+-- res: 3
 
 
 
 -- 14. Visualizza i nomi degli studenti e i corsi che hanno frequentato nel 2022.
--- res: 
+-- res: 6
 
 
 
 -- 15. Elencare i professori con i loro corsi e i relativi dipartimenti.
--- res: 
+-- res: 10
 
 
-
+-- DELETED
 -- 16. Trova i corsi senza studenti iscritti.
 -- res: 
+-- DELETED
 
-
-
+-- DELETED
 -- 17. Mostra i corsi con più di 5 iscritti.
 -- res: 
+-- DELETED
 
-
-
+-- DELETED
 -- 18. Elenca tutti i professori che non hanno corsi assegnati.
 -- res: 
-
+-- DELETED
 
 
 -- 19. Visualizza i corsi con il maggior numero di studenti.
--- res: 
+-- res: 1 (3)
 
 
-
+-- DELETED
 -- 20. Trova gli studenti che sono iscritti a più di 3 corsi.
 -- res: 
-
+-- DELETED
 
 
